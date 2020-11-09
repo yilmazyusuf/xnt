311c6e4ce7504f71e019ca2cd1c09655c5570cfe
yusuf <y.yilmaz@hotmail.com.tr>
2020-11-10 01:45:11 +0300

    sql file moved

--- docker/database/sql/init.sql
+++ docker/database/sql/init.sql
@@ -2,6 +2,7 @@ CREATE TABLE posts
 (
     id          INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     title       VARCHAR(100) NOT NULL,
+    slug        VARCHAR(100) NOT NULL,
     content     TEXT,
     is_headline TINYINT(3),
     user_id     INTEGER(11) UNSIGNED,
